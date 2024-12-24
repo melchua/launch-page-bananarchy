8d0c5cf12c452406c4496082962e384f02d4908e
Mel Chua <mchuam@gmail.com>
2024-12-24 22:51:40 +0700

    Remove posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,26 +3,26 @@
 	import '@fontsource/londrina-solid';
 	import '@fontsource/poppins';
 
-	import posthog from 'posthog-js';
-	import { beforeNavigate, afterNavigate } from '$app/navigation';
-	import { browser } from '$app/environment';
+	// import posthog from 'posthog-js';
+	// import { beforeNavigate, afterNavigate } from '$app/navigation';
+	// import { browser } from '$app/environment';
 
 	export const prerender = true;
 
-	export const load = async () => {
-		if (browser) {
-			posthog.init('phc_dQL4w1lcT9beEs5Dfpc6qt7vDez8OR4OombqZzu2cGl', {
-				api_host: 'https://us.i.posthog.com',
-				person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-			});
-		}
-		return;
-	};
+	// export const load = async () => {
+	// 	if (browser) {
+	// 		posthog.init('phc_dQL4w1lcT9beEs5Dfpc6qt7vDez8OR4OombqZzu2cGl', {
+	// 			api_host: 'https://us.i.posthog.com',
+	// 			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+	// 		});
+	// 	}
+	// 	return;
+	// };
 
-	if (browser) {
-		beforeNavigate(() => posthog.capture('$pageleave'));
-		afterNavigate(() => posthog.capture('$pageview'));
-	}
+	// if (browser) {
+	// 	beforeNavigate(() => posthog.capture('$pageleave'));
+	// 	afterNavigate(() => posthog.capture('$pageview'));
+	// }
 
 	let { children } = $props();
 </script>
