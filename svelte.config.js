7f12402cd44e7c1868e75882c787fe9ce672e62f
Mel Chua <mchuam@gmail.com>
2024-12-24 23:25:23 +0700

    Update

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,6 @@
+// Notes on how to deploy an SSG to Cloudflare Pages
+// https://developers.cloudflare.com/pages/framework-guides/deploy-a-svelte-site/
+
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -11,11 +14,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ pages: 'build', assets: 'build', fallback: null, precompress: false }),
-		prerender: {
-			// This ensures all routes are prerendered
-			default: true
-		}
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
+		})
+		// prerender: {
+		// 	// This ensures all routes are prerendered
+		// 	default: true
+		// }
 	}
 };
 
