9eaa4203ecd3ed72a4dd12589c9e106fbd4f4e8a
Mel Chua <mchuam@gmail.com>
2025-04-04 00:12:00 -0700

    Update mailer text styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,10 +7,11 @@
 >
 	<div class="flex flex-col justify-between gap-4 sm:w-full sm:flex-row lg:w-[90%] xl:w-[72%]">
 		<span>
-			<h1 class="text-2xl text-white">We're launching soon!</h1>
+			<h1 class="py-2 text-2xl text-white">We're launching soon!</h1>
 			<p class="w-full text-sm text-white">
-				Sign up now to be the <span>first to know</span> when we launch and receive a special offer for
-				our backers.
+				Sign up now to be the <span class="font-black text-bapurple">first to know</span> when we
+				launch and
+				<span class="font-black text-bapurple">receive a special offer</span> for our backers.
 			</p>
 		</span>
 		<MailerForm />
