dfa966dd49e105d1636ad2523ef989a3476f9b84
Mel Chua <mchuam@gmail.com>
2026-04-16 13:05:44 +0700

    Remove animation for mobile

--- src/routes/thankyou/+page.svelte
+++ src/routes/thankyou/+page.svelte
@@ -116,22 +116,6 @@
 		class="absolute inset-0 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-bapurple via-purple-700/50 to-transparent"
 	></div>
 	<div class="relative w-full">
-		<!-- <div
-			class="fixed left-0 right-0 top-0 z-50 flex w-full justify-end bg-black/95 px-4 py-3 text-center text-white shadow-lg backdrop-blur-sm sm:hidden md:shadow-none"
-		>
-			<div class="flex w-full justify-center">
-				<stripe-buy-button
-					buy-button-id={`${process.env.NODE_ENV === 'production' ? 'buy_btn_1T4GwKAsNXUFfbI0Q2bcV4Yt' : 'buy_btn_1T4H7GPPIJAaixg8JZXorcI7'}`}
-					publishable-key={`${process.env.NODE_ENV === 'production' ? 'pk_live_51R4sxhAsNXUFfbI0fAl27QrPAtQ2nvehYLChkv76Nc5C371lHgLn89PLXuqWToXANsCiMQNlgxvRkaAfZLSZRZDS00kRHtBo8W' : 'pk_test_51R4sxoPPIJAaixg8YvZtmNEcmVmzitoMlK9DAMS8LI7AwwlLs4F1w5usO9DUeqs8ifXZdDf2BRtMjDHUQlZZj24O00Cod4QbJz'}	`}
-					on:click={() => handleBuyButtonClick('main')}
-					on:keydown={(e: KeyboardEvent) => e.key === 'Enter' && handleBuyButtonClick('main')}
-					role="button"
-					tabindex="0"
-				>
-				</stripe-buy-button>
-			</div>
-		</div> -->
-
 		<!-- Thank you confirmation section -->
 		<div class="mx-auto w-full max-w-4xl px-4 pt-2 text-center">
 			<p class="text-sm text-white/70 md:text-base">
@@ -190,7 +174,7 @@
 				<enhanced:img
 					src={cards}
 					alt="Monkey Business expansion cards"
-					class="mx-auto w-full max-w-xl animate-bounce-in"
+					class="mx-auto w-full max-w-xl sm:animate-bounce-in"
 					sizes="(min-width: 768px) 600px, 90vw"
 				/>
 			</div>
